c5cf474546c480bd04c7157e7aa87dd9c32df5e0
ProgrammingTutor22 <chocwafernz@gmail.com>
2024-06-25 20:31:11 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeclaringVariables
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int numA = 49;
+            int numB = 35;
+            char Yes = 'Y';
+            char No = 'N';
+            String Result = "Pass";
+            Console.WriteLine(numA + numB);
+            Console.WriteLine(Yes +" or "+ No);
+            Console.WriteLine(Result);
+            Console.ReadLine();
+        }
+    }
+}
